a463a3b666e963e96821d44bda731bb9eb7d046e
abhishek332 <abhishek.porwal@deuexsolutions.com>
2023-03-08 23:04:12 +0530

    responsive navbar complete

--- src/app/components/navbar/navbar.component.js
+++ src/app/components/navbar/navbar.component.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import LegendToggleIcon from '@mui/icons-material/LegendToggle';
 import './navbar.css';
-import { Link } from "react-router-dom";
-import { useContext } from "react";
-import { Context } from "../../utils/context-provider";
-import { connectToWallet } from "../../utils/load-contracts";
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { Context } from '../../utils/context-provider';
+import { connectToWallet } from '../../utils/load-contracts';
 
 function Navbar() {
 	const [modal, setModal] = useState(false);
@@ -31,36 +31,53 @@ function Navbar() {
 	};
 
 	const handleButtonClick = () => {
-        if(address !== undefined) {
+		if (address !== undefined) {
 			disConnect();
 		} else {
-            connectWallet();
+			connectWallet();
 		}
-    };
+	};
 
 	return (
-		<div className="navbar center">
-			<div className="center">
-				<LegendToggleIcon
-					onClick={() => setModal(!modal)}
-					className={`toggle-btn ${modal ? 'flip-toggle' : 'reverse-toggle'}`}
-				/>
-				<h3 className="navbar-brand">Crypto State</h3>
-				<div className="nav-links center">
-					<Link className="nav-link" to="/">
-						Home
-					</Link>
-					<Link className="nav-link" to="/properties">
-						Properties
-					</Link>
+		<>
+			<div className="navbar center">
+				<div className="center">
+					<LegendToggleIcon
+						onClick={() => setModal(!modal)}
+						className={`toggle-btn ${modal ? 'flip-toggle' : 'reverse-toggle'}`}
+					/>
+					<h3 className="navbar-brand">Crypto State</h3>
+					<div className="nav-links center">
+						<Link className="nav-link" to="/">
+							Home
+						</Link>
+						<Link className="nav-link" to="/properties">
+							Properties
+						</Link>
+					</div>
+				</div>
+				<div className="center">
+					<button onClick={handleButtonClick}>
+						{!address ? 'Connect to Wallet' : 'Disconnect to Wallet'}
+					</button>
 				</div>
 			</div>
-			<div className="center">
-				<button onClick={handleButtonClick}>
-					{!address ? 'Connect to Wallet' : 'Disconnect to Wallet'}
-				</button>
+			<div
+				className="nav-modal center"
+				style={
+					modal
+						? { transform: 'translateX(0)' }
+						: { transform: 'translateX(-100%)' }
+				}
+			>
+				<Link className="nav-link" to="/">
+					Home
+				</Link>
+				<Link className="nav-link" to="/properties">
+					Properties
+				</Link>
 			</div>
-		</div>
+		</>
 	);
 }
 
