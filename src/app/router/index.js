5f5caa8f562bd4c77f3309b526948e037a9f75e2
abhishek332 <abhishek.porwal@deuexsolutions.com>
2023-03-12 22:10:23 +0530

    some changes

--- src/app/router/index.js
+++ src/app/router/index.js
@@ -1,29 +1,25 @@
-import Navbar from "../components/navbar/navbar.component";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "../pages/home-page/home-page.component";
-import Footer from "../components/footer/footer.component";
-import Property from "../pages/property-page/property-page.component";
-import Register from "../pages/register-page/register-page.component";
-import RecipeReviewCard from "../components/Detiles_page/Eetiles";
+import Navbar from '../components/navbar/navbar.component';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from '../pages/home-page/home-page.component';
+import Footer from '../components/footer/footer.component';
+import Property from '../pages/property-page/property-page.component';
+import Register from '../pages/register-page/register-page.component';
+import RecipeReviewCard from '../components/Detiles_page/Eetiles';
 
 // you can import like this
-const Router = ()=>{
-  return (
-    <div className="container">
-      <BrowserRouter>
-        <Navbar />
-        <div className="page-container">
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/moreInfo" element={<RecipeReviewCard />} />
-          <Route path="/properties" element={<Property />} />
-          <Route path="/Register" element={<Register />} />
-        </Routes>
-          </div>
-        <Footer />
-      </BrowserRouter>
-    </div>
-  );
-}
+const Router = () => {
+	return (
+		<BrowserRouter>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />}></Route>
+				<Route path="/moreInfo" element={<RecipeReviewCard />} />
+				<Route path="/properties" element={<Property />} />
+				<Route path="/Register" element={<Register />} />
+			</Routes>
+			<Footer />
+		</BrowserRouter>
+	);
+};
 
 export default Router;
