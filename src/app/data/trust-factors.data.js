9e7d27130ab54f780edb948a3096aa3fb6e1c39e
abhishek332 <abhishek.porwal@deuexsolutions.com>
2023-03-25 20:31:21 +0530

    replace dynamic assets to static assets and some minor changes

--- src/app/data/trust-factors.data.js
+++ src/app/data/trust-factors.data.js
@@ -1,7 +1,11 @@
-export const TrustFactors= {
-    data: [
+import Solidity from '../assets/solidity.jpeg';
+import Ethereum from '../assets/ethereum.jpeg';
+import IndianGovt from '../assets/indian-govt.jpeg';
+
+export const TrustFactors = {
+	data: [
 		{
-			img: 'https://as1.ftcdn.net/v2/jpg/02/10/17/26/1000_F_210172606_04K3sGd9NqyMiH6rZfmxjUkDCChrHxy7.jpg',
+			img: Solidity,
 			keyPoints: [
 				'Written in Solidity',
 				'Audited by  Deloitte',
@@ -9,7 +13,7 @@ export const TrustFactors= {
 			],
 		},
 		{
-			img: 'https://thumbs.dreamstime.com/b/illustration-vector-ethereum-logo-long-shadow-illustration-vector-ethereum-logo-long-shadow-cryptocurrency-113135110.jpg',
+			img: Ethereum,
 			keyPoints: [
 				'Security of Ethereum',
 				'Validate Buy Proof of Stack',
@@ -17,12 +21,12 @@ export const TrustFactors= {
 			],
 		},
 		{
-			img: 'https://logopeople.in/blog/wp-content/uploads/2013/01/government-of-india.jpg',
+			img: IndianGovt,
 			keyPoints: [
 				'Operated by GOVT OF INDIA',
 				'Department of Real State GOI',
 				'Total Listed Property 1M+',
 			],
 		},
-	]
-}
+	],
+};
