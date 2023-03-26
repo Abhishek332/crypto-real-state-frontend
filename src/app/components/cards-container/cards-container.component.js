297925c6f4102e1748cfb92ca88f076c4421d061
abhishek332 <abhishek.porwal@deuexsolutions.com>
2023-03-27 00:41:51 +0530

    make more flexible to cards container

--- src/app/components/cards-container/cards-container.component.js
+++ src/app/components/cards-container/cards-container.component.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './cards-container.css';
 
-function CardsContainer({heading, dataList, child: Child}) {
+function CardsContainer({ heading, dataList, child: Child }) {
 	return (
 		<section>
 			{heading && <h1>{heading}</h1>}
 			<div className="cards-wrapper center">
 				{dataList.map((data) => (
-					<Child {...data} />
+					<Child key={Math.random().toString()} {...data} />
 				))}
 			</div>
 		</section>
