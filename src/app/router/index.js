5b236e89d4feadcf9274b60b70618da108aa0655
Akhilleshgoswami <agiri5375@gmail.com>
2023-04-03 10:43:18 +0530

    write deployment script for firebase and add minor changes

--- src/app/router/index.js
+++ src/app/router/index.js
@@ -13,11 +13,12 @@ import { ARBI_TESTNET_CHAIN_ID } from '../utils/constants';
 import { getAccount } from '@wagmi/core';
 import SwitchNetwork from '../components/switchNetwork/SwitchNetwork';
 import { Context } from '../utils/context-provider';
+import AlertDialog from '../components/alert-dialog/alert-dialog.component';
 
 // you can import like this
 const Router = () => {
 	const [chainId, setChainID] = useState(ARBI_TESTNET_CHAIN_ID);
-	const { setAddress } = useContext(Context);
+	const { setAddress,isMetamaskInstalled } = useContext(Context);
 	const checkChainChanged = async () => {
 		const netowrkChainId = await getChainId();
 		if (netowrkChainId !== ARBI_TESTNET_CHAIN_ID) {
@@ -43,8 +44,14 @@ const Router = () => {
 		checkChainChanged();
 	}, []);
 
+
+
+
+
 	return (
+	
 		<div className="container">
+	
 			{chainId !== ARBI_TESTNET_CHAIN_ID ? (
 				<div>
 					<SwitchNetwork />
@@ -69,6 +76,12 @@ const Router = () => {
 					</BrowserRouter>
 				</div>
 			)}
+			    <AlertDialog
+				title="install Metamask"
+				text="Please install Metamask"
+				isDialogOpen={!isMetamaskInstalled}
+				
+			/>
 		</div>
 	);
 };
--- src/app/utils/context-provider/index.js
+++ src/app/utils/context-provider/index.js
@@ -8,22 +8,40 @@ const Context = createContext({});
 const ContextProvider = ({ children }) => {
 	const [contractInstance, setContract] = useState();
 	const [address, setAddress] = useState();
+	const [isMetamaskInstalled,setisMetamaskInstalled] = useState(true)
 	const load = async () => {
-		const contract = await loadContracts();
-		setContract(contract);
-		const acc = await window.ethereum.request({
-			method: 'eth_requestAccounts',
-		});
-		setAddress(acc[0]);
-	};
+	
+		if(window.ethereum){
+			try {
+				const contract = await loadContracts();
+				setContract(contract);
+				const acc = await window.ethereum.request({
+					method: 'eth_requestAccounts',
+				});
+				setAddress(acc[0]);
+			} catch (error) {
+				console.log(error)
+			}
+		}
+		else{
+			openPopupForMetamaskConnection()
+		}
+		
 
+	};
+	const openPopupForMetamaskConnection = () => {
+		setisMetamaskInstalled(false);
+		setTimeout(() => {
+			setisMetamaskInstalled(true);
+		}, 12000);
+	};
 	useEffect(() => {
 		load();
 	}, []);
 
 	return (
 		<Context.Provider
-			value={{ contractInstance, address, setAddress, setContract }}
+			value={{ contractInstance, address, setAddress, setContract ,isMetamaskInstalled}}
 		>
 			{children}
 		</Context.Provider>
